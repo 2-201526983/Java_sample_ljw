925a557c201f83a12fea6d0b2f6a064b93c2eb0a
admin <admin@host.docker.internal>
2022-10-28 14:18:50 +0900

    p

---
+++ coffeePos/src/main/test2.java
@@ -0,0 +1,9 @@
+package main;
+
+import main.MainUI;
+
+public class test2 {
+    public static void main(String[] args) {
+    	MainUI.getInstance();
+    }
+}
--- my_test2/src/asset/DBConnectionMgr.java
+++ my_test2/src/asset/DBConnectionMgr.java
@@ -1,4 +1,4 @@
-package src.asset;
+package asset;
 
 
 /**
