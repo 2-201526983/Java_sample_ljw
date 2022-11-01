38fed51e2c70ad0871693375ae7fb947408d3867
LJW <tyranitars@naver.com>
2022-11-01 10:33:05 +0900

    Delete ch16_1026 directory

--- ch16_1026/src/ch16_1026/Come.java
+++
@@ -1,9 +0,0 @@
-package ch16_1026;
-
-public class Come {
-public void come() {
-      while(true){
-       System.out.println("come");
-      }
-   } //come() 메소드내에서 무한루프를 돌면서 문자열"come"를 출력
-}
--- ch16_1026/src/ch16_1026/Go.java
+++
@@ -1,9 +0,0 @@
-package ch16_1026;
-
-public class Go {
-	   public void go() {
-	         while(true){
-	          System.out.println("go");
-	         }
-	      }
-	  } // go() 메소드내에서 무한루프를 돌면서 문자열"go"를 출력
